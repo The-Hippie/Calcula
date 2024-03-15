c85d34c0adb504af0ac542676cafb2d61c5b8ecb
HR  Hippie <158214025+The-Hippie@users.noreply.github.com>
2024-03-15 14:48:31 -0500

    Actualizar MainActivity.java

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -6,7 +6,24 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    EditText edtVal1, edtVal2;
+    TextView txtResult;
+    Button btnCalcular;
+
+    public void CalcularSuma(View view){
+    EditText edtVal1 = findViemByid(R.id.edtVal1);
+    EditText edtVal2 = findViewById(R.id.edtVal2);
+    TextView txtResult = findViewByid(R.id.txtResult);
+
+int value1 = Integer.parenInt(edtVal1.getText().toString());
+int value2 = Integer.parenInt(edtVal2.getText().toString());
+
+int sum = value1 + value2;
+
+textResult.setText(String.valueOf(sum));
+} 
+  
+ @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
